88642bb234214927c783945dbf0c77ef7aaaf1c5
mbeltrame <martin.beltrame@mercadolibre.com>
2022-01-11 17:27:56 -0300

    Add custom card

--- app/src/main/java/com/example/poc_architecture/views/adapters/viewholders/itemSearch/ItemSearchViewHolder.kt
+++ app/src/main/java/com/example/poc_architecture/views/adapters/viewholders/itemSearch/ItemSearchViewHolder.kt
@@ -7,11 +7,11 @@ import com.example.poc_architecture.R
 import com.example.poc_architecture.models.ComponentDTO
 import com.example.poc_architecture.models.ItemSearchComponentDTO
 import com.example.poc_architecture.utils.ICallbackShowMore
-import com.example.poc_architecture.views.custom.ShowMoreTextView
 import com.example.poc_architecture.utils.ViewMode
 import com.example.poc_architecture.views.IListenerUpdate
 import com.example.poc_architecture.views.adapters.viewholders.ComponentsViewHolder
 import com.example.poc_architecture.views.custom.HighlightView
+import com.example.poc_architecture.views.custom.ShowMoreTextView
 
 class ItemSearchViewHolder(
     itemView: View,
@@ -24,6 +24,8 @@ class ItemSearchViewHolder(
     private val showMore = itemView.findViewById<ShowMoreTextView>(R.id.text_item_search)
     private val highlightView = itemView.findViewById<HighlightView>(R.id.highlight_view)
     private val btnUpdate = itemView.findViewById<Button>(R.id.btn_update)
+//    private val textView = itemView.findViewById<TextView>(R.id.text_view)
+//    private val container = itemView.findViewById<LinearLayout>(R.id.container_text_view)
 
     override fun bind(componentDTO: ComponentDTO, position: Int?) {
         val itemSearchComponentDTO = (componentDTO as ItemSearchComponentDTO)
@@ -38,5 +40,7 @@ class ItemSearchViewHolder(
                 listenerUpdate?.updateView(pos)
             }
         }
+//        textView.text = itemSearchComponentDTO.text
+//        container.setShadow(R.color.background_image, R.dimen.radius, R.dimen.elevation, Gravity.BOTTOM, R.color.white)
     }
 }
